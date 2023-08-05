6e3517a003e1556aa95906035b0134768347cfcd
godot23 <jasonanavon@gmail.com>
2023-08-04 21:52:00 -0400

    i put my api key in as public, which isnt great. removed and cancelled the api key.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 ///event listener should wrap around all the fetch requests
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=5d7f52a9c8f4587b04685e80174a55f3")
+// add API key to keep it secure.
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=******")
     .then(response => response.json())
     .then(cityData => {
         let cityReturn = cityData[0];
         console.log(cityReturn.lat);
         console.log(cityReturn.lon);
 
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityReturn.lat}&lon=${cityReturn.lon}&appid=5d7f52a9c8f4587b04685e80174a55f3`)
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityReturn.lat}&lon=${cityReturn.lon}&appid=******`)
     })
 
 
